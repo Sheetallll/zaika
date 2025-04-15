b4564ea4999d653929208b3ad2ebaff005c361b1
sheetallll <sheetallodhi28@gmail.com>
2025-04-15 22:42:55 +0530

    Fixed screen alignments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,57 +1,3 @@
-// import { Suspense } from "react";
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import { UILoader } from "./components/loaders/index";
-// import { DashboardLayout } from "./layouts/dashboard";
-// import { AddRecipe, Home, More, MyRecipes } from "./pages/Dashboard";
-// import { ErrorPage } from "./pages/Error";
-// import { Landing } from "./pages/Landing";
-// import { AuthenticationContext } from "./context/auth-context";
-
-// function App() {
-//   const router = createBrowserRouter([
-//     {
-//       path: "/",
-//       element: <Landing />,
-//       errorElement: <ErrorPage />,
-//     },
-
-//     {
-//       path: "/dashboard",
-//       element: <DashboardLayout />,
-//       errorElement: <ErrorPage />,
-//       children: [
-//         {
-//           path: "/dashboard/",
-//           element: <Home />,
-//         },
-//         {
-//           path: "/dashboard/addrecipe",
-//           element: <AddRecipe />,
-//         },
-//         {
-//           path: "/dashboard/myrecipes",
-//           element: <MyRecipes />,
-//         },
-//         {
-//           path: "/dashboard/recipe/:id",
-//           element: <More />,
-//         },
-//       ],
-//     },
-//   ]);
-//   return (
-//     <div className="container h-[100vh] w-[100vw]">
-//       <Suspense fallback={<UILoader />}>
-//         <RouterProvider router={router} fallbackElement={<UILoader />} />
-//       </Suspense>
-//       <AuthenticationContext.Provider value={{ loading: false, onLogin: async () => {} }}>
-//   <Landing />
-// </AuthenticationContext.Provider>
-//     </div>
-//   );
-// }
-
-// export default App;
 import { Suspense } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { UILoader } from "./components/loaders/index";
@@ -105,7 +51,7 @@ function App() {
   ]);
 
   return (
-    <div className="container h-screen w-screen">
+    <div className="min-h-screen w-full overflow-x-hidden">
       <AuthenticationContext.Provider value={authContextValue}>
         <Suspense fallback={<UILoader />}>
           <RouterProvider router={router} fallbackElement={<UILoader />} />
